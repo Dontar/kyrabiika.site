765256222bbd4921a983585a99ed5a2ccad075e2
Kaloyan Arsov <kaloyan.arsov@gmail.com>
2021-09-14 21:16:28 +0000

    server changes

---
+++ server/src/connection.ts
@@ -0,0 +1,69 @@
+import { model, Schema } from 'mongoose';
+
+export interface User {
+    firstName: string;
+    lastName: string;
+    mail: string;
+    password: string;
+    admin?: boolean;
+    phone?: string;
+    address?: string;
+    orders: Order[];
+}
+
+export interface MenuItem {
+    name: string;
+    category: string;
+    price: number;
+}
+
+type OrderItem = { item: MenuItem; count: number; };
+
+export interface Order {
+    items: OrderItem[];
+    date: Date;
+    progress: string;
+    user: User;
+}
+
+export const MenuItemModel = model<MenuItem>('MenuItem', new Schema<MenuItem>({
+    name: { type: String, required: true, unique: true },
+    category: { type: String, required: true },
+    price: { type: Number, required: true },
+}));
+
+export const OrderModel = model<Order>('Order', new Schema<Order>({
+    items: [{
+        item: { type: Schema.Types.ObjectId, ref: 'MenuItem' },
+        count: Number
+    }],
+    date: Date,
+    progress: { type: String, enum: ['Prepearing', 'Enroute', 'Delivered'] },
+    user: { type: Schema.Types.ObjectId, ref: 'User' }
+}));
+
+export const UserModel = model<User>('User', new Schema<User>({
+    firstName: String,
+    lastName: String,
+    mail: { type: String, required: true, unique: true },
+    phone: String,
+    address: String,
+    orders: [{ type: Schema.Types.ObjectId, ref: 'Order' }],
+    admin: Boolean,
+    password: { type: String, required: true }
+}));
+
+
+MenuItemModel.insertMany([
+    { category: 'банички и соленки',       name: 'баничка с козе сирене и спанак',      price: 1 },
+    { category: 'банички и соленки',       name: 'баничка със сирене и подправки',      price: 1 },
+    { category: 'курабийки и бисквити',    name: 'курабийки',                           price: 1 },
+    { category: 'мини тарталети и кишове', name: 'тарталети с кленов сироп и орех',     price: 1 },
+    { category: 'мини тарталети и кишове', name: 'тарталети с крем маскарпоне',         price: 1 },
+    { category: 'мини тарталети и кишове', name: 'тарталети с ментов крем',             price: 1 },
+    { category: 'мини тарталети и кишове', name: 'чийзкейк в гнездо с горски плодове',  price: 1 },
+    { category: 'мъфини и къпкайкове',     name: 'банофи пай',                          price: 1 },
+    { category: 'мъфини и къпкайкове',     name: 'къпкейк с шамфъстък',                 price: 1 },
+    { category: 'мъфини и къпкайкове',     name: 'мъфин с морков',                      price: 1 },
+    { category: 'торти',                   name: 'тарт с плодове',                      price: 1 }
+]);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,64 @@
-import express from 'express';
+import express, { RequestHandler } from 'express';
+import { AddressInfo } from 'net';
+import cors from 'cors';
+import morgan from 'morgan';
+import { config as envConfig } from 'dotenv';
+import cluster from 'cluster';
+import { cpus } from 'os';
+import { connect, disconnect } from 'mongoose';
 
-const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+envConfig();
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+if (cluster.isPrimary) {
+
+    onExit(cleanUpWorkers);
+
+    const cpuCount = Number(process.env.WORKERS ?? cpus().length);
+    console.info(`Starting ${cpuCount} workers...`);
+    for (let i = 0; i < cpuCount; i++) {
+        cluster.fork();
+    }
+
+} else {
+
+    onExit(cleanUpDatabase);
+
+    const app = express();
+
+    app.use(cors({
+        origin: process.env.CORS_ORIGIN
+    }));
+
+    app.use(morgan('common'));
+
+    const port = Number(process.env.SERVER_PORT);
+    const host = process.env.SERVER_HOST ?? 'localhost';
+
+    const server = app.listen(port, host, async () => {
+        await connect(process.env.SERVER_DB ?? 'mongodb://root:example@db:27017/?authSource=admin&readPreference=primary&ssl=false');
+        const { address, port } = server.address() as AddressInfo;
+        console.info(`${cluster.worker!.id} Listening on ${address}:${port}`);
+    });
+}
+
+function catchAsyncErrors(handler: (...params: Parameters<RequestHandler>) => Promise<void>): RequestHandler {
+    return function (req, res, next) {
+        handler(req, res, next).catch(next);
+    };
+}
+
+function cleanUpWorkers() {
+    for (const [, worker] of Object.entries(cluster.workers!)) {
+        worker?.kill();
+    }
+}
+
+async function cleanUpDatabase() {
+    await disconnect();
+}
+
+function onExit(cb: (...args: unknown[]) => void) {
+    const exitEvents = ['exit', 'SIGHUP', 'SIGINT', 'SIGINT', 'SIGTERM'];
+    exitEvents.forEach(e => process.on(e, cb));
+}
