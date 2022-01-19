cf63945a32f30b6fd89cc978a530432802f0bd1d
Kaloyan Arsov <kaloyan.arsov@gmail.com>
2022-01-19 23:12:13 +0000

    Added fetch wrapper (rest client)

---
+++ lib/utils/rest-client.ts
@@ -0,0 +1,40 @@
+type HTTPMethod = "get" | "post" | "put" | "patch" | "del";
+const HTTPMethods: HTTPMethod[] = ["get", "post", "put", "patch", "del"];
+
+export class FetchError extends Error {
+  constructor(message: string, public response: Response, public data?: { message: string }) {
+    super(message);
+    this.name = "FetchError";
+  }
+}
+
+function parseResult(res: Response) {
+  const type = res.headers.get("Content-Type")!;
+  switch (true) {
+    case /json/.test(type): return res.json();
+    case /text/.test(type): return res.text();
+    default: return res.blob();
+  }
+}
+
+type RestClientAPI = Record<HTTPMethod, <T = any>(url: string, data?: unknown) => Promise<T>>;
+
+const rest = {} as RestClientAPI;
+
+HTTPMethods.forEach(method => {
+  rest[method] = async (url: string, data?: unknown) => {
+    const res = await fetch(url, {
+      method,
+      headers: data ? {
+        "Content-Type": "application/json"
+      } : undefined,
+      body: data ? JSON.stringify(data) : undefined
+    });
+    if (res.ok) {
+      return await parseResult(res);
+    }
+    throw new FetchError(res.statusText, res, await res.json());
+  };
+});
+
+export default rest;
