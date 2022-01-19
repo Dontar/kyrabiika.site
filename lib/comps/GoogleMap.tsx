f2e7a9ea6273d32e6e53b0117b7b83d71cf5b482
Kaloyan Arsov <kaloyan.arsov@gmail.com>
2022-01-19 23:11:13 +0000

    Optimising GoogleMap component

--- lib/comps/GoogleMap.tsx
+++ lib/comps/GoogleMap.tsx
@@ -3,7 +3,7 @@ import { useEffect, useRef } from "react";
 export interface GoogleMapOptions /* extends React.PropsWithChildren */ {
   pin?: google.maps.LatLngLiteral;
   address?: string;
-  onNewPosition?: (pos: google.maps.LatLngLiteral, address: string) => void;
+  onNewPosition?: (pos: google.maps.LatLngLiteral, address?: string) => void;
 }
 
 export default function GoogleMap(props: GoogleMapOptions) {
@@ -19,39 +19,44 @@ export default function GoogleMap(props: GoogleMapOptions) {
 
   async function findGeo(request: google.maps.GeocoderRequest) {
     const geocode = new google.maps.Geocoder();
-    const { results: [result] } = await geocode.geocode({...request, region: "bg-BG"});
+    const { results: [result] } = await geocode.geocode({ ...request, region: "bg-BG" });
     return result;
   }
 
   function setPin(pos: google.maps.LatLngLiteral) {
     if (!pin.current) {
       pin.current = new google.maps.Marker({
-        map: map.current,
-        position: pos
+        map: map.current
       });
-    } else {
-      pin.current.setPosition(pos);
-      map.current?.panTo(pos);
     }
+    pin.current.setPosition(pos);
+    map.current?.panTo(pos);
   }
 
-  async function updateInfo() {
+  async function updateInfo(result?: Partial<google.maps.GeocoderResult>) {
     if (!info.current) {
       info.current = new google.maps.InfoWindow();
     }
-    const result = await findGeo({
-      location: pin.current?.getPosition()
-    });
-    info.current.setContent(result.formatted_address);
-    info.current.open(map.current, pin.current);
-    return result.formatted_address;
+    try {
+      if (!result) {
+        result = await findGeo({
+          location: pin.current?.getPosition()
+        });
+      }
+      info.current.setContent(result.formatted_address);
+      info.current.open(map.current, pin.current);
+      return result.formatted_address;
+    } catch (e) {
+      console.error(e);
+      return "";
+    }
   }
 
   useEffect(() => {
     if (ref.current && !map.current) {
       map.current = new google.maps.Map(ref.current, {
         center: props.pin || defaultPos,
-        zoom: 12,
+        zoom: 14,
         clickableIcons: false,
         fullscreenControl: false,
         mapTypeControl: false,
@@ -59,11 +64,10 @@ export default function GoogleMap(props: GoogleMapOptions) {
         streetViewControl: false
       });
       map.current.addListener("click", async (e: google.maps.MapMouseEvent) => {
-        if (e.latLng) {
-          const pos = e.latLng.toJSON();
-          setPin(pos);
-          const address = await updateInfo();
-          props.onNewPosition && props.onNewPosition(pos, address);
+        if (e.latLng && props.onNewPosition) {
+          const location = e.latLng.toJSON();
+          const { formatted_address } = await findGeo({ location });
+          props.onNewPosition(location, formatted_address);
         }
       });
     }
@@ -72,14 +76,12 @@ export default function GoogleMap(props: GoogleMapOptions) {
   useEffect(() => {
     if (props.pin) {
       setPin(props.pin);
-      updateInfo();
-    }
-    if (props.address) {
-      findGeo({address: props.address}).then(result => {
+      updateInfo(props.address ? { formatted_address: props.address } : undefined);
+    } else if (typeof props.address === "string") {
+      findGeo({ address: props.address }).then(result => {
         setPin(result.geometry.location.toJSON());
-        updateInfo();
-      });
-
+        return updateInfo(result);
+      }).catch(console.error);
     }
   });
 
