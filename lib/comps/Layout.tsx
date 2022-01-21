7993762ca5e9436f5113312f11ce51625ee95382
Kaloyan Arsov <kaloyan.arsov@gmail.com>
2022-01-21 09:02:17 +0000

    Added user avatar & cart

--- lib/comps/Layout.tsx
+++ lib/comps/Layout.tsx
@@ -5,26 +5,31 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Row from "react-bootstrap/Row";
 import Nav from "react-bootstrap/Nav";
+import Popover from "react-bootstrap/Popover";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import ListGroup from "react-bootstrap/ListGroup";
+import Badge from "react-bootstrap/Badge";
+
+import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons/faShoppingCart";
+
 
 import Link from "next/link";
 import { useRouter } from "next/router";
+import Avatar from "react-avatar";
 
 import useSWR from "swr";
 import { LoggedInUser, SiteConfig } from "../db/DbTypes";
 import { useOrderContext } from "./OrderContext";
 import rest from "../utils/rest-client";
 
-export interface LayoutProps {
-  navLinks?: React.ReactNode;
-}
-
-export default function Layout({ navLinks, children }: React.PropsWithChildren<LayoutProps>) {
+export default function Layout({ children }: React.PropsWithChildren<unknown>) {
   const { data } = useSWR<SiteConfig>("/api/config", url => fetch(url).then(r => r.json()));
 
   const router = useRouter();
   const order = useOrderContext();
 
-  const logOut = async (e: any) => {
+  const logOut = async (e: React.MouseEvent<Element>) => {
     e.preventDefault();
     order.clear();
     await order.setUser(rest.get<LoggedInUser>("/api/logout"), false);
@@ -39,31 +44,43 @@ export default function Layout({ navLinks, children }: React.PropsWithChildren<L
             <span>КуРабиЙкА</span>
             <span className="ms-2 navbar-brand-office">office</span>
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-            {navLinks}
-            <Nav>
-              {order.user?.isLoggedIn === true ? (
-                <>
-                  <Link href="/profile" passHref={true}>
-                    <Nav.Link>{order.userName}</Nav.Link>
-                  </Link>
-                  <Link href="/admin" passHref={true}>
-                    <Nav.Link>Admin</Nav.Link>
-                  </Link>
-                  <Link href="/api/logout" passHref={true}>
-                    <Nav.Link onClick={(e: any) => logOut(e)}>Logout</Nav.Link>
-                  </Link>
-                </>
-              ) : (
-                <Link href="/login" passHref={true}>
-                  <Nav.Link>Login / Register</Nav.Link>
+
+          <Nav className="gap-2 flex-nowrap">
+            {order.user?.isLoggedIn === true ? (
+              <>
+                <Link href="/order" passHref>
+                  <Nav.Link className="d-flex p-0 border rounded-circle bg-secondary text-white justify-content-center position-relative" style={{ width: "36px", height: "36px" }}>
+                    <Icon icon={faShoppingCart} className="align-self-center"/>
+                    {!!order.items.length && (<Badge pill bg="danger" className="position-absolute top-0 start-100 translate-middle">{order.items.length}</Badge>)}
+                  </Nav.Link>
                 </Link>
+                <OverlayTrigger trigger="click" placement="bottom-start" overlay={(
+                  <Popover id="popover-basic">
+                    <Popover.Body>
 
-              )}
-            </Nav>
+                      <ListGroup variant="flush">
+                        <Link href="/profile" passHref>
+                          <ListGroup.Item action>Profile</ListGroup.Item>
+                        </Link>
+                        <Link href="/admin" passHref>
+                          <ListGroup.Item action>Admin</ListGroup.Item>
+                        </Link>
+                        <ListGroup.Item action onClick={e => logOut(e)}>Logout</ListGroup.Item>
+                      </ListGroup>
+                    </Popover.Body>
+                  </Popover>
+                )}>
+                  <Avatar name={order.userName} email={order.user.mail} round size="36px" className="my-auto" />
+                </OverlayTrigger>
 
-          </Navbar.Collapse>
+              </>
+            ) : (
+              <Link href="/login" passHref={true}>
+                <Nav.Link>Login / Register</Nav.Link>
+              </Link>
+
+            )}
+          </Nav>
         </Container>
       </Navbar>
       {children}
